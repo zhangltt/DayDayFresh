7ca8369b40ffa6aeaf7a84fef0b5659598ffe1f1
张磊 <593000965@qq.com>
2017-05-31 09:52:33 +0800

    配置数据库和时区

--- DayDayFresh/DayDayFresh/settings.py
+++ DayDayFresh/DayDayFresh/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'DayDayFresh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'daydayfresh',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD':'mysql',
     }
 }
 
@@ -86,9 +90,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
